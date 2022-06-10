cbdc97db7c1097298b7b40be1f8a6d696c66126e
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2022-06-10 14:01:57 +0300

    productcarts position fix

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -1,12 +1,19 @@
-import { Group } from "@mantine/core";
+import { Group, GroupPosition } from "@mantine/core";
 import { ProductCard } from "../ProductCard/ProductCard";
 import { useViewportSize } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 
 export function ProductList() {
   const { width } = useViewportSize();
+  const [position, setPosition] = useState<GroupPosition>();
+
+  useEffect(() => {
+    setPosition(width > 800 ? "left" : "center");
+  }, []);
+
   return (
     <>
-      <Group position={width ? (width > 800 ? "left" : "center") : "left"}>
+      <Group position={position}>
         <ProductCard img="./item1.png" />
         <ProductCard img="./item2.png" />
         <ProductCard img="./item3.png" />
